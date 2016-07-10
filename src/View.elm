79f57bb96dbc55ab72ec7a2cfc272ea9fbd82d40
not-much-io <kristo.koert@gmail.com>
2016-07-10 15:23:49 +0300

    Hacking1

--- src/View.elm
+++ src/View.elm
@@ -4,8 +4,101 @@ import Html exposing (..)
 import Html.Attributes exposing (href, class, style)
 import Material.Scheme
 import Material.Button as Button
+import Material.Table as Table
+import Material.Toggles as Toggles
 import Update exposing (Msg)
 import Model exposing (Model)
+
+
+type alias Data =
+    { material : String
+    , quantity : String
+    , unitPrice : String
+    }
+
+
+sortedData : List Data
+sortedData =
+    List.repeat 10
+        (Data "Material" "10" "100")
+
+
+table : List Data -> Html Msg
+table list =
+    Table.table []
+        [ Table.thead []
+            [ Table.tr []
+                [ Table.th []
+                    [ text "Proficiency" ]
+                , Table.th [ Table.numeric ]
+                    [ text "Score" ]
+                , Table.th []
+                    [ text "Skill" ]
+                ]
+            ]
+        , Table.tbody []
+            (list
+                |> List.map
+                    (\item ->
+                        Table.tr []
+                            [ Table.td [] [ text item.material ]
+                            , Table.td [ Table.numeric ] [ text item.quantity ]
+                            , Table.td [ Table.numeric ] [ text item.unitPrice ]
+                            ]
+                    )
+            )
+        ]
+
+
+type Skill
+    = Acrobatics
+    | AnimalHandling
+    | Arcana
+    | Athletics
+    | Deception
+    | History
+    | Insight
+    | Intimidation
+    | Investigation
+    | Medicine
+    | Nature
+    | Perception
+    | Performance
+    | Persuasion
+    | Religion
+    | SleightOfHand
+    | Stealth
+    | Survival
+
+
+type alias SkillScore =
+    { proficiency : Bool
+    , score : Int
+    , skill : Skill
+    }
+
+
+skillScores : List SkillScore
+skillScores =
+    List.repeat 10 (SkillScore False 0 Acrobatics)
+
+
+skills : Model -> List SkillScore -> Html Msg
+skills model skillScores =
+    div []
+        (List.map
+            (\ ( id, skill ) ->
+                Toggles.checkbox Update.MDL
+                    [ id ]
+                    model.mdl
+                    [ Toggles.value skill.proficiency ]
+                    [ text "0 Acrobatics" ]
+            )
+            (List.map2 (,)
+                [1..10]
+                skillScores
+            )
+        )
 
 
 view : Model -> Html Msg
@@ -38,5 +131,7 @@ view model =
             model.mdl
             [ Button.onClick Update.Reset ]
             [ text "Reset" ]
+        , table sortedData
+        , skills model skillScores
         ]
         |> Material.Scheme.top
